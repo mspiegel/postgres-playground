7b4dabcc6d7eb472b60892b5f886e911ff11dd59
Michael Spiegel <michael.m.spiegel@gmail.com>
2018-09-18 18:47:52 -0400

    Add coding assignment code framework to git

---
+++ median.c
@@ -0,0 +1,46 @@
+#include <postgres.h>
+#include <fmgr.h>
+
+#ifdef PG_MODULE_MAGIC
+PG_MODULE_MAGIC;
+#endif
+
+PG_FUNCTION_INFO_V1(median_transfn);
+
+/*
+ * Median state transfer function.
+ *
+ * This function is called for every value in the set that we are calculating
+ * the median for. On first call, the aggregate state, if any, needs to be
+ * initialized.
+ */
+Datum
+median_transfn(PG_FUNCTION_ARGS)
+{
+    MemoryContext agg_context;
+
+    if (!AggCheckCallContext(fcinfo, &agg_context))
+        elog(ERROR, "median_transfn called in non-aggregate context");
+
+    PG_RETURN_NULL();
+}
+
+PG_FUNCTION_INFO_V1(median_finalfn);
+
+/*
+ * Median final function.
+ *
+ * This function is called after all values in the median set has been
+ * processed by the state transfer function. It should perform any necessary
+ * post processing and clean up any temporary state.
+ */
+Datum
+median_finalfn(PG_FUNCTION_ARGS)
+{
+    MemoryContext agg_context;
+
+    if (!AggCheckCallContext(fcinfo, &agg_context))
+        elog(ERROR, "median_finalfn called in non-aggregate context");
+
+    PG_RETURN_NULL();
+}
---
+++ test/sql/median.sql
@@ -0,0 +1,51 @@
+CREATE TABLE intvals(val int, color text);
+
+-- Test empty table
+SELECT median(val) FROM intvals;
+
+-- Integers with odd number of values
+INSERT INTO intvals VALUES
+       (1, 'a'),
+       (2, 'c'),
+       (9, 'b'),
+       (7, 'c'),
+       (2, 'd'),
+       (-3, 'd'),
+       (2, 'e');
+
+SELECT * FROM intvals ORDER BY val;
+SELECT median(val) FROM intvals;
+
+-- Integers with NULLs and even number of values
+INSERT INTO intvals VALUES
+       (99, 'a'),
+       (NULL, 'a'),
+       (NULL, 'e'),
+       (NULL, 'b'),
+       (7, 'c'),
+       (0, 'd');
+
+SELECT * FROM intvals ORDER BY val;
+SELECT median(val) FROM intvals;
+
+-- Text values
+CREATE TABLE textvals(val text, color int);
+
+INSERT INTO textvals VALUES
+       ('erik', 1),
+       ('mat', 3),
+       ('rob', 8),
+       ('david', 9),
+       ('lee', 2);
+
+SELECT * FROM textvals ORDER BY val;
+SELECT median(val) FROM textvals;
+
+-- Test large table with timestamps
+CREATE TABLE timestampvals (val timestamptz);
+
+INSERT INTO timestampvals(val)
+SELECT TIMESTAMP 'epoch' + (i * INTERVAL '1 second')
+FROM generate_series(0, 100000) as T(i);
+
+SELECT median(val) FROM timestampvals;
