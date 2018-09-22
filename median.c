ab44f9d10984bba64734955edc5d87b8dbda91c6
Michael Spiegel <michael.m.spiegel@gmail.com>
2018-09-22 09:46:29 -0400

    Apply pg_bsd_indent formatting

--- median.c
+++ median.c
@@ -10,13 +10,12 @@
 PG_MODULE_MAGIC;
 #endif
 
-typedef struct SortInfo
-{
-    FmgrInfo lt, eq;
-	Oid			fncollation;
-} SortInfo;
+typedef struct SortInfo {
+	FmgrInfo	lt, eq;
+	Oid		fncollation;
+}		SortInfo;
 
-int cmp_datums(const void *p1, const void *p2, void *context);
+int		cmp_datums(const void *p1, const void *p2, void *context);
 
 PG_FUNCTION_INFO_V1(median_transfn);
 PG_FUNCTION_INFO_V1(median_finalfn);
@@ -29,61 +28,59 @@ PG_FUNCTION_INFO_V1(median_finalfn);
  * initialized.
  */
 Datum
-median_transfn(PG_FUNCTION_ARGS)
-{
-    MemoryContext agg_context;
-    MedianState *median_state;
-    bytea	   *state = (PG_ARGISNULL(0) ? NULL : PG_GETARG_BYTEA_P(0));
-    Datum	   val_datum = (PG_ARGISNULL(1) ? 0 : PG_GETARG_DATUM(1));
+median_transfn(PG_FUNCTION_ARGS) {
+	MemoryContext	agg_context;
+	MedianState    *median_state;
+	bytea	       *state = (PG_ARGISNULL(0) ? NULL : PG_GETARG_BYTEA_P(0));
+	Datum		val_datum = (PG_ARGISNULL(1) ? 0 : PG_GETARG_DATUM(1));
 
-    if (!AggCheckCallContext(fcinfo, &agg_context))
-        elog(ERROR, "median_transfn called in non-aggregate context");
+	if (!AggCheckCallContext(fcinfo, &agg_context))
+		elog(ERROR, "median_transfn called in non-aggregate context");
 
-    if (PG_ARGISNULL(1))
-        PG_RETURN_BYTEA_P(state);
+	if (PG_ARGISNULL(1))
+		PG_RETURN_BYTEA_P(state);
 
-	if (state == NULL)
-	{
-		Size arrsize = sizeof(Datum) * 8;
+	if (state == NULL) {
+		Size		arrsize = sizeof(Datum) * 8;
 		state = MemoryContextAllocZero(agg_context, MEDIAN_STATE_HEADER + arrsize);
 		SET_VARSIZE(state, MEDIAN_STATE_HEADER + arrsize);
-        ((MedianState*) state)->type_oid = get_fn_expr_argtype(fcinfo->flinfo, 1);
+		((MedianState *) state)->type_oid = get_fn_expr_argtype(fcinfo->flinfo, 1);
 	}
 
-    median_state = (MedianState*) state;
+	median_state = (MedianState *) state;
 
-    if (median_state->len == MEDIAN_STATE_CAPACITY(state))
-    {
-        Size oldsize = sizeof(Datum) * median_state->len;
-		Size newsize = oldsize * 2;
-        state = repalloc(state, MEDIAN_STATE_HEADER + newsize);
+	if (median_state->len == MEDIAN_STATE_CAPACITY(state)) {
+		Size		oldsize = sizeof(Datum) * median_state->len;
+		Size		newsize = oldsize * 2;
+		state = repalloc(state, MEDIAN_STATE_HEADER + newsize);
 		SET_VARSIZE(state, MEDIAN_STATE_HEADER + newsize);
-        median_state = (MedianState*) state;
-    }
+		median_state = (MedianState *) state;
+	}
 
-    median_state->data[median_state->len] = val_datum;
-    median_state->len++;
+	median_state->data[median_state->len] = val_datum;
+	median_state->len++;
 
-    PG_RETURN_BYTEA_P(state);
+	PG_RETURN_BYTEA_P(state);
 }
 
-int cmp_datums(const void *p1, const void *p2, void *context)
+int
+cmp_datums(const void *p1, const void *p2, void *context)
 {
-    // TODO: replace calls to "<" and "=" with a single call to compare() function
-    SortInfo *sort_info = (SortInfo*) context;
-    Datum left = *((Datum*) p1);
-    Datum right = *((Datum*) p2);
-    bool lt, eq;
+//TODO:	replace calls to "<" and "=" with a single call to compare() function
+		SortInfo * sort_info = (SortInfo *) context;
+	Datum		left = *((Datum *) p1);
+	Datum		right = *((Datum *) p2);
+	bool		lt, eq;
 
-    lt = DatumGetBool(FunctionCall2Coll(&sort_info->lt, sort_info->fncollation, left, right));
-    if (lt)
-        return -1;
+	lt = DatumGetBool(FunctionCall2Coll(&sort_info->lt, sort_info->fncollation, left, right));
+	if (lt)
+		return -1;
 
-    eq = DatumGetBool(FunctionCall2Coll(&sort_info->eq, sort_info->fncollation, left, right));
-    if (eq)
-        return 0;
+	eq = DatumGetBool(FunctionCall2Coll(&sort_info->eq, sort_info->fncollation, left, right));
+	if (eq)
+		return 0;
 
-    return 1;
+	return 1;
 }
 
 /*
@@ -94,25 +91,24 @@ int cmp_datums(const void *p1, const void *p2, void *context)
  * post processing and clean up any temporary state.
  */
 Datum
-median_finalfn(PG_FUNCTION_ARGS)
-{
-    MemoryContext agg_context;
-    MedianState *median_state;
-    SortInfo	sort_info;
-    Oid			type_oid,lt_op,lt_regproc,eq_op,eq_regproc;
+median_finalfn(PG_FUNCTION_ARGS) {
+	MemoryContext	agg_context;
+	MedianState    *median_state;
+	SortInfo	sort_info;
+	Oid		type_oid, lt_op, lt_regproc, eq_op, eq_regproc;
 
 
-    if (!AggCheckCallContext(fcinfo, &agg_context))
-        elog(ERROR, "median_finalfn called in non-aggregate context");
+	if (!AggCheckCallContext(fcinfo, &agg_context))
+		elog(ERROR, "median_finalfn called in non-aggregate context");
 
 	if (PG_ARGISNULL(0))
 		PG_RETURN_NULL();
 
 	median_state = (MedianState *) PG_GETARG_POINTER(0);
-    type_oid = median_state->type_oid;
+	type_oid = median_state->type_oid;
 
 	if (!OidIsValid(type_oid))
-        elog(ERROR, "could not determine the type of the elements");
+		elog(ERROR, "could not determine the type of the elements");
 
 	lt_op = OpernameGetOprid(list_make1(makeString("<")), type_oid, type_oid);
 	if (!OidIsValid(lt_op))
@@ -130,9 +126,9 @@ median_finalfn(PG_FUNCTION_ARGS)
 		elog(ERROR, "could not find the procedure for the = operator for type %d", type_oid);
 	fmgr_info_cxt(eq_regproc, &sort_info.eq, fcinfo->flinfo->fn_mcxt);
 
-    sort_info.fncollation = fcinfo->fncollation;
-    qsort_r(median_state->data, median_state->len, sizeof(Datum), cmp_datums, &sort_info );
+	sort_info.fncollation = fcinfo->fncollation;
+	qsort_r(median_state->data, median_state->len, sizeof(Datum), cmp_datums, &sort_info);
 
-    // TODO: return the arithmetic mean of neighbors when even number of elements
-	PG_RETURN_DATUM(median_state->data[median_state->len / 2]);
+//TODO:	return the arithmetic mean of neighbors when even number of elements
+		PG_RETURN_DATUM(median_state->data[median_state->len / 2]);
 }
--- median2.c
+++ median2.c
@@ -6,10 +6,10 @@
 
 #include "median.h"
 
-void median2_swap(Datum* a, Datum* b);
-bool boolean_func(Datum left, Datum right, FmgrInfo *finfo, Oid fncollation);
-int32 median2_partition(Datum arr[], int32 left, int32 right, FmgrInfo *finfo, Oid fncollation);
-Datum quickselect(Datum arr[], int32 left, int32 right, int32 k, FmgrInfo *finfo, Oid fncollation);
+void		median2_swap(Datum * a, Datum * b);
+bool		boolean_func(Datum left, Datum right, FmgrInfo * finfo, Oid fncollation);
+int32		median2_partition(Datum arr[], int32 left, int32 right, FmgrInfo * finfo, Oid fncollation);
+Datum		quickselect(Datum arr[], int32 left, int32 right, int32 k, FmgrInfo * finfo, Oid fncollation);
 
 PG_FUNCTION_INFO_V1(median2_transfn);
 PG_FUNCTION_INFO_V1(median2_finalfn);
@@ -22,88 +22,90 @@ PG_FUNCTION_INFO_V1(median2_finalfn);
  * initialized.
  */
 Datum
-median2_transfn(PG_FUNCTION_ARGS)
-{
-    MemoryContext agg_context;
-    MedianState *median_state;
-    bytea	   *state = (PG_ARGISNULL(0) ? NULL : PG_GETARG_BYTEA_P(0));
-    Datum	   val_datum = (PG_ARGISNULL(1) ? 0 : PG_GETARG_DATUM(1));
+median2_transfn(PG_FUNCTION_ARGS) {
+	MemoryContext	agg_context;
+	MedianState    *median_state;
+	bytea	       *state = (PG_ARGISNULL(0) ? NULL : PG_GETARG_BYTEA_P(0));
+	Datum		val_datum = (PG_ARGISNULL(1) ? 0 : PG_GETARG_DATUM(1));
 
-    if (!AggCheckCallContext(fcinfo, &agg_context))
-        elog(ERROR, "median_transfn called in non-aggregate context");
+	if (!AggCheckCallContext(fcinfo, &agg_context))
+		elog(ERROR, "median_transfn called in non-aggregate context");
 
-    if (PG_ARGISNULL(1))
-        PG_RETURN_BYTEA_P(state);
+	if (PG_ARGISNULL(1))
+		PG_RETURN_BYTEA_P(state);
 
-	if (state == NULL)
-	{
-		Size arrsize = sizeof(Datum) * 8;
+	if (state == NULL) {
+		Size		arrsize = sizeof(Datum) * 8;
 		state = MemoryContextAllocZero(agg_context, MEDIAN_STATE_HEADER + arrsize);
 		SET_VARSIZE(state, MEDIAN_STATE_HEADER + arrsize);
-        ((MedianState*) state)->type_oid = get_fn_expr_argtype(fcinfo->flinfo, 1);
+		((MedianState *) state)->type_oid = get_fn_expr_argtype(fcinfo->flinfo, 1);
 	}
 
-    median_state = (MedianState*) state;
+	median_state = (MedianState *) state;
 
-    if (median_state->len == MEDIAN_STATE_CAPACITY(state))
-    {
-        Size oldsize = sizeof(Datum) * median_state->len;
-		Size newsize = oldsize * 2;
-        state = repalloc(state, MEDIAN_STATE_HEADER + newsize);
+	if (median_state->len == MEDIAN_STATE_CAPACITY(state)) {
+		Size		oldsize = sizeof(Datum) * median_state->len;
+		Size		newsize = oldsize * 2;
+		state = repalloc(state, MEDIAN_STATE_HEADER + newsize);
 		SET_VARSIZE(state, MEDIAN_STATE_HEADER + newsize);
-        median_state = (MedianState*) state;
-    }
+		median_state = (MedianState *) state;
+	}
 
-    median_state->data[median_state->len] = val_datum;
-    median_state->len++;
+	median_state->data[median_state->len] = val_datum;
+	median_state->len++;
 
-    PG_RETURN_BYTEA_P(state);
+	PG_RETURN_BYTEA_P(state);
 }
 
-void median2_swap(Datum* a, Datum* b) 
-{ 
-    Datum t = *a; 
-    *a = *b; 
-    *b = t; 
-}
-
-bool boolean_func(Datum left, Datum right, FmgrInfo *finfo, Oid fncollation)
+void
+median2_swap(Datum * a, Datum * b)
 {
-    return DatumGetBool(FunctionCall2Coll(finfo, fncollation, left, right));
+
+	Datum		t = *a;
+	*a = *b;
+	*b = t;
 }
 
-int32 median2_partition(Datum arr[], int32 left, int32 right, FmgrInfo *finfo, Oid fncollation)
+bool
+boolean_func(Datum left, Datum right, FmgrInfo * finfo, Oid fncollation)
 {
-    Datum val = arr[right];
-    int32 i = left;
-    for (int32 j = left; j <= right - 1; j++) {
-        if (boolean_func(arr[j], val, finfo, fncollation)) {
-            median2_swap(&arr[i], &arr[j]);
-            i++;
-        }
-    }
-    median2_swap(&arr[i], &arr[right]);
-    return i;
+	return DatumGetBool(FunctionCall2Coll(finfo, fncollation, left, right));
 }
 
-Datum quickselect(Datum arr[], int32 left, int32 right, int32 k, FmgrInfo *finfo, Oid fncollation)
+int32
+median2_partition(Datum arr[], int32 left, int32 right, FmgrInfo * finfo, Oid fncollation)
 {
-    if (k > 0 && k <= right - left + 1) {
- 
-        int32 index = median2_partition(arr, left, right, finfo, fncollation);
- 
-        if (index - left == k - 1)
-            return arr[index];
- 
-        if (index - left > k - 1) 
-            return quickselect(arr, left, index - 1, k, finfo, fncollation);
- 
-        return quickselect(arr, index + 1, right, 
-                            k - index + left - 1, finfo, fncollation);
-    }
+	Datum		val = arr[right];
+	int32		i = left;
+	for (int32 j = left; j <= right - 1; j++) {
+		if (boolean_func(arr[j], val, finfo, fncollation)) {
+			median2_swap(&arr[i], &arr[j]);
+			i++;
+		}
+	}
+	median2_swap(&arr[i], &arr[right]);
+	return i;
+}
 
-    elog(ERROR, "kth element selection is out of bounds"); 
-    return 0;
+Datum
+quickselect(Datum arr[], int32 left, int32 right, int32 k, FmgrInfo * finfo, Oid fncollation)
+{
+	if (k > 0 && k <= right - left + 1) {
+
+		int32		index = median2_partition(arr, left, right, finfo, fncollation);
+
+		if (index - left == k - 1)
+			return arr[index];
+
+		if (index - left > k - 1)
+			return quickselect(arr, left, index - 1, k, finfo, fncollation);
+
+		return quickselect(arr, index + 1, right,
+				   k - index + left - 1, finfo, fncollation);
+	}
+
+	elog(ERROR, "kth element selection is out of bounds");
+	return 0;
 }
 
 /*
@@ -114,25 +116,24 @@ Datum quickselect(Datum arr[], int32 left, int32 right, int32 k, FmgrInfo *finfo
  * post processing and clean up any temporary state.
  */
 Datum
-median2_finalfn(PG_FUNCTION_ARGS)
-{
-    MemoryContext agg_context;
-    MedianState *median_state;
-    FmgrInfo    lte_finfo;
-    Oid			type_oid,lte_op,lte_regproc;
-    Datum       median;
+median2_finalfn(PG_FUNCTION_ARGS) {
+	MemoryContext	agg_context;
+	MedianState    *median_state;
+	FmgrInfo	lte_finfo;
+	Oid		type_oid, lte_op, lte_regproc;
+	Datum		median;
 
-    if (!AggCheckCallContext(fcinfo, &agg_context))
-        elog(ERROR, "median_finalfn called in non-aggregate context");
+	if (!AggCheckCallContext(fcinfo, &agg_context))
+		elog(ERROR, "median_finalfn called in non-aggregate context");
 
 	if (PG_ARGISNULL(0))
 		PG_RETURN_NULL();
 
 	median_state = (MedianState *) PG_GETARG_POINTER(0);
-    type_oid = median_state->type_oid;
+	type_oid = median_state->type_oid;
 
 	if (!OidIsValid(type_oid))
-        elog(ERROR, "could not determine the type of the elements");
+		elog(ERROR, "could not determine the type of the elements");
 
 	lte_op = OpernameGetOprid(list_make1(makeString("<=")), type_oid, type_oid);
 	if (!OidIsValid(lte_op))
@@ -142,8 +143,8 @@ median2_finalfn(PG_FUNCTION_ARGS)
 		elog(ERROR, "could not find the procedure for the <= operator for type %d", type_oid);
 	fmgr_info_cxt(lte_regproc, &lte_finfo, fcinfo->flinfo->fn_mcxt);
 
-    // TODO: return the arithmetic mean of neighbors when even number of elements
-    median = quickselect(median_state->data, 0, median_state->len, median_state->len / 2, &lte_finfo, fcinfo->fncollation);
+//TODO:	return the arithmetic mean of neighbors when even number of elements
+		median = quickselect(median_state->data, 0, median_state->len, median_state->len / 2, &lte_finfo, fcinfo->fncollation);
 
 	PG_RETURN_DATUM(median);
 }
